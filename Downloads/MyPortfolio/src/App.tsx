565eac5a7213c8eeffd90dfcb41e160d0c423c4a
Rahul Raj <rahul1492002@gmail.com>
2025-07-07 11:07:12 +0530

    Initial commit for new Portfolio repo

--- Downloads/MyPortfolio/src/App.tsx
+++ Downloads/MyPortfolio/src/App.tsx
@@ -21,7 +21,9 @@ export function App() {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href') || '');
+          const href = (e.currentTarget as HTMLAnchorElement).getAttribute('href') || '';
+         const target = document.querySelector(href);
+        //const target = document.querySelector(this.getAttribute('href') || '');
         if (target) {
           window.scrollTo({
             top: target.offsetTop,
